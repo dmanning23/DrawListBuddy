2ca4a94af3bf479d92ea743a54e010d074902613
Dan Manning <dmanning@datafinch.com>
2014-10-25 22:25:22 -0400

    ouya proj

--- Source/DrawList.cs
+++ Source/DrawList.cs
@@ -1,9 +1,8 @@
+using GameTimer;
+using Microsoft.Xna.Framework;
+using RenderBuddy;
 using System.Collections.Generic;
 using System.Diagnostics;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using GameTimer;
-using RenderBuddy;
 
 namespace DrawListBuddy
 {
